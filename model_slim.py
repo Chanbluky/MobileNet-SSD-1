9b1783b20efd4146d22edba430c400eaff07518e
邬小阳 <1441615466@qq.com>
2018-06-06 21:49:12 +0800

    model slim file to remove softmax layer

--- model_slim.py
+++ model_slim.py
@@ -1,7 +1,19 @@
 #! /usr/bin/env python
-# -*- coding:utf-8 -*-
+# -*- coding:utf-8 -*- 
 
 '''
-remove fc parameter from caffemodel
+remove the softmax layer after training process, if possibile, remove the data layer also
+only preserve the feature embedding layer
 '''
 
+import sys 
+sys.path.append('/home/wujiyang/caffe/python')
+import caffe
+import numpy as np
+import matplotlib.pyplot as plt
+
+deploy_model = '../FaceModels/amsoftmax-20/amsoftmax_20_deploy_mirror_normalize.prototxt'
+caffe_model = '../FaceModels/amsoftmax-20/amsoftmax_20_vggface2.caffemodel'
+net = caffe.Net(deploy_model, caffe_model, caffe.TEST)  
+
+net.save('../FaceModels/amsoftmax-20/final.caffemodel')
